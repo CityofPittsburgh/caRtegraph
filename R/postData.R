f436082cedcc868a99a756e3a5b439ab3f8b5f72
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2019-05-14 10:30:54 -0400

    Post records

--- R/postData.R
+++ R/postData.R
@@ -26,7 +26,7 @@ cgPut <- function(class, body, un, pw, org) {
   print(httr::content(P))
 }
 
-#' Title
+#' Delete
 #'
 #' @param class cartegraph class or class and attachment ie 'cgSigns_cgAttachmentsClass'
 #' @param oid Oid of the object you wish to delete
@@ -46,4 +46,30 @@ cgDelete <- function(class, oid, un, pw, org) {
   D <- httr::DELETE(url, httr::authenticate(un, pw, type = "basic"))
 
   print(httr::content(D))
+}
+
+#' Create new record(s) in Cartegraph
+#'
+#' @param class cartegraph class or class and attachment ie 'cgSigns_cgAttachmentsClass'
+#' @param body DataFrame/tibble to be turned into JSON object
+#' @param un api username
+#' @param pw api password
+#' @param org orginization API ID ie 'PittsburghPA'
+#'
+#' @return http status
+#' @export
+#'
+#' @examples \dontrun{
+#' df$cgSignsClass <- data.frame(IDField = "Sign-50",
+#'      AddressNumberField = 1765)
+#'
+#' cgPOST("cgSignsClass", df,"fakeUN", "fakePW", "AnyTownUSA")
+#' }
+cgPost <- function(class, body, un, pw, org) {
+  json <- jsonlite::toJSON(body)
+
+  url <- paste0("https://cgweb06.cartegraphoms.com/", org, "/api/v1/classes/", class)
+
+  P <- httr::POST(url, httr::authenticate(un, pw, type = "basic"), body = json)
+  print(httr::content(P))
 }
